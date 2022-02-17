07f92b52440313789d8d7326f0f6c72c13247ae4
Enes Bayraktar <enes.bayraktar@outlook.com.tr>
2022-02-18 02:38:37 +0300

    Add: TextView to WebViewActivitiy

--- app/src/main/kotlin/com/enesthedev/webview/views/WebViewActivity.kt
+++ app/src/main/kotlin/com/enesthedev/webview/views/WebViewActivity.kt
@@ -1,7 +1,8 @@
-package com.enesthedev.webview
+package com.enesthedev.webview.views
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.enesthedev.webview.R
 
 class WebViewActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {

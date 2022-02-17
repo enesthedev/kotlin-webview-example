1a77fae40587b27218c7ca8ec4995b9ea89c9f2e
Enes Bayraktar <enes.bayraktar@outlook.com.tr>
2022-02-18 02:40:32 +0300

    Add: SplashScreen fade-out animation
    Add: splash_screen_iv to Splash Screen Layout

--- app/src/main/kotlin/com/enesthedev/webview/views/SplashScreenActivity.kt
+++ app/src/main/kotlin/com/enesthedev/webview/views/SplashScreenActivity.kt
@@ -1,9 +1,11 @@
-package com.enesthedev.webview
+package com.enesthedev.webview.views
 
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
+import com.enesthedev.webview.R
+
+import kotlinx.android.synthetic.main.activity_splash_screen.splash_screen_iv
 
 @Suppress("DEPRECATION")
 class SplashScreenActivity : AppCompatActivity() {
@@ -11,9 +13,10 @@ class SplashScreenActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
 
-        Handler().postDelayed({
+        splash_screen_iv.animate().setDuration(1000).alpha(1f).withEndAction {
             startActivity(Intent(this, WebViewActivity::class.java))
+            overridePendingTransition(androidx.constraintlayout.widget.R.anim.abc_fade_in, androidx.constraintlayout.widget.R.anim.abc_fade_out)
             finish()
-        }, 1500)
+        }
     }
 }
